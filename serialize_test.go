5096a738981d3c2e662bd977fc2c5f94b10b355f
j0nimost <john.nyingi@live.com>
2021-06-18 07:45:28 +0300

    Added arrays of float types

--- serialize_test.go
+++ serialize_test.go
@@ -33,6 +33,7 @@ type SliceType struct {
 	Books      []string
 	TaxNumbers []uint32
 	Prices     []int32
+	Distances  []float32
 }
 
 func TestWithOnlyStrings(t *testing.T) {
@@ -114,13 +115,14 @@ func TestWithFloats(t *testing.T) {
 }
 
 func TestWithSlices(t *testing.T) {
-	exp := "{\"books\":[\"Old McDonald Had A Farm\",\"Calculus I\"],\"taxNumbers\":[123455,235555,6643233,664433],\"prices\":[-231,334,64645,23232,55,4,232,54,232,544,3232,343]}"
+	exp := "{\"books\":[\"Old McDonald Had A Farm\",\"Calculus I\"],\"taxNumbers\":[123455,235555,6643233,664433],\"prices\":[-231,334,64645,23232,55,4,232,54,232,544,3232,343],\"distances\":[12.5,454.56,343.2546,45434.562]}"
 
 	book := []string{"Old McDonald Had A Farm", "Calculus I"}
 	taxNumbers := []uint32{123455, 235555, 6643233, 664433}
 	prices := []int32{-231, 334, 64645, 23232, 55, 4, 232, 54, 232, 544, 3232, 343}
+	distances := []float32{12.5, 454.56, 343.2546, 45434.562}
 
-	act, err := Serialize(SliceType{Books: book, TaxNumbers: taxNumbers, Prices: prices})
+	act, err := Serialize(SliceType{Books: book, TaxNumbers: taxNumbers, Prices: prices, Distances: distances})
 
 	if err != nil {
 		t.Error(err.Error())
