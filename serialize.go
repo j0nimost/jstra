78bc131c6596f8098eaab6fa2744df9b47a32c07
John Nyingi <john.nyingi@live.com>
2021-06-27 22:04:19 +0300

    Added Struct Supports

--- serialize.go
+++ serialize.go
@@ -8,69 +8,85 @@ import (
 	"strings"
 )
 
+type jstraSerialize struct {
+	json string
+}
+
 func Serialize(str interface{}) (string, error) {
-	var json string
+	js := newJstraSerialize()
+
+	return js.Serializer(str)
+
+}
+
+func newJstraSerialize() *jstraSerialize {
+	return &jstraSerialize{json: ""}
+}
+
+func (js *jstraSerialize) Serializer(str interface{}) (string, error) {
 
 	t := reflect.TypeOf(str)
 	v := reflect.ValueOf(str)
 	n := t.NumField()
 
-	// TODO: Support Struct Pointers
+	// TODO Support Struct Pointers
 	if t.Kind() != reflect.Struct {
 		err := errors.New("type passed is not of type Struct or Struct Pointer")
 		return "", err
 	}
 
-	json += "{"
+	js.json += "{"
 	// iterate through the fields
 	for i := 0; i < n; i++ {
 		tt := t.Field(i)
 		vv := v.Field(i)
 
-		json += "\"" + jsonFormarter(tt.Name) + "\":"
+		js.json += "\"" + jsonFormarter(tt.Name) + "\":"
 
 		switch tt.Type.Kind() {
 		case reflect.String:
-			json += "\"" + vv.String() + "\""
+			js.json += "\"" + vv.String() + "\""
 		case reflect.Bool:
-			json += fmt.Sprintf("%v", vv)
+			js.json += fmt.Sprintf("%v", vv)
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			json += fmt.Sprintf("%v", vv)
+			js.json += fmt.Sprintf("%v", vv)
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			json += fmt.Sprintf("%v", vv)
+			js.json += fmt.Sprintf("%v", vv)
 		case reflect.Float32, reflect.Float64:
-			json += fmt.Sprintf("%v", vv)
+			js.json += fmt.Sprintf("%v", vv)
 		case reflect.Slice:
 			st := tt.Type.Elem()
 
-			json += "["
+			js.json += "["
 
 			for x := 0; x < vv.Len(); x++ {
 				switch st.Kind() {
 				case reflect.String:
-					json += fmt.Sprintf("\"%v\"", vv.Index(x))
+					js.json += fmt.Sprintf("\"%v\"", vv.Index(x))
 				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-					json += fmt.Sprintf("%v", vv.Index(x))
+					js.json += fmt.Sprintf("%v", vv.Index(x))
 				case reflect.Float32, reflect.Float64:
-					json += fmt.Sprintf("%v", vv.Index(x))
+					js.json += fmt.Sprintf("%v", vv.Index(x))
 				}
 				if x < vv.Len()-1 {
-					json += ","
+					js.json += ","
 				}
 			}
 
-			json += "]"
+			js.json += "]"
 
+		case reflect.Struct:
+			js.Serializer(vv.Interface())
 		}
 
 		if i < n-1 {
-			json += ","
+			js.json += ","
 		}
 	}
 
-	json += "}"
+	js.json += "}"
 
-	return json, nil
+	return js.json, nil
 }
 
 func jsonFormarter(s string) string {
@@ -82,8 +98,4 @@ func jsonFormarter(s string) string {
 	r := b[1:]
 	lc := bytes.ToLower([]byte{b[0]})
 	return string(bytes.Join([][]byte{lc, r}, nil))
-}
-
-func jsonStringArray(arr []string) {
-
 }
