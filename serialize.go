8ab482ffecad81319f2ed6a83f76fd8aa3d2512b
John Nyingi <john.nyingi@live.com>
2021-09-07 09:22:15 +0300

    fixed: Merge error

--- serialize.go
+++ serialize.go
@@ -9,12 +9,10 @@ import (
 )
 
 type jstraSerialize struct {
-	json string
+	json strings.Builder
 }
 
 func Serialize(str interface{}) (string, error) {
-
-	var json strings.Builder
 
 	js := newJstraSerialize()
 
@@ -23,11 +21,10 @@ func Serialize(str interface{}) (string, error) {
 }
 
 func newJstraSerialize() *jstraSerialize {
-	return &jstraSerialize{json: ""}
+	return &jstraSerialize{json: strings.Builder{}}
 }
 
 func (js *jstraSerialize) serializer(str interface{}) (string, error) {
-
 
 	t := reflect.TypeOf(str)
 	v := reflect.ValueOf(str)
@@ -35,7 +32,7 @@ func (js *jstraSerialize) serializer(str interface{}) (string, error) {
 	if t.Kind() == reflect.Slice {
 		st := t.Elem()
 
-		js.json += "["
+		js.json.WriteString("[")
 		for x := 0; x < v.Len(); x++ {
 			switch st.Kind() {
 			case reflect.Struct:
@@ -45,48 +42,11 @@ func (js *jstraSerialize) serializer(str interface{}) (string, error) {
 				return "", err
 			}
 			if x < v.Len()-1 {
-				js.json += ","
+				js.json.WriteString(",")
 			}
 
-
-	json.WriteString("{")
-	// iterate through the fields
-	for i := 0; i < n; i++ {
-		tt := t.Field(i)
-		vv := v.Field(i)
-
-		json.WriteString("\"" + jsonFormarter(tt.Name) + "\":")
-
-		switch tt.Type.Kind() {
-		case reflect.String:
-			json.WriteString("\"" + vv.String() + "\"")
-		case reflect.Bool:
-			json.WriteString(fmt.Sprintf("%v", vv))
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			json.WriteString(fmt.Sprintf("%v", vv))
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			json.WriteString(fmt.Sprintf("%v", vv))
-		case reflect.Float32, reflect.Float64:
-			json.WriteString(fmt.Sprintf("%v", vv))
-		case reflect.Slice:
-			st := tt.Type.Elem()
-
-			json.WriteString("[")
-
-			for x := 0; x < vv.Len(); x++ {
-				switch st.Kind() {
-				case reflect.String:
-					json.WriteString(fmt.Sprintf("\"%v\"", vv.Index(x)))
-				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-					json.WriteString(fmt.Sprintf("%v", vv.Index(x)))
-				case reflect.Float32, reflect.Float64:
-					json.WriteString(fmt.Sprintf("%v", vv.Index(x)))
-				}
-				if x < vv.Len()-1 {
-					json.WriteString(",")
-
 		}
-		js.json += "]"
+		js.json.WriteString("]")
 
 	} else if t.Kind() == reflect.Ptr {
 		p := reflect.Indirect(v)
@@ -95,75 +55,61 @@ func (js *jstraSerialize) serializer(str interface{}) (string, error) {
 	} else if t.Kind() == reflect.Struct {
 		n := t.NumField()
 
-		js.json += "{"
+		js.json.WriteString("{")
 		// iterate through the fields
 		for i := 0; i < n; i++ {
 			tt := t.Field(i)
 			vv := v.Field(i)
 
-			js.json += "\"" + jsonFormarter(tt.Name) + "\":"
+			js.json.WriteString("\"" + jsonFormarter(tt.Name) + "\":")
 
 			switch tt.Type.Kind() {
 			case reflect.String:
-				js.json += "\"" + vv.String() + "\""
+				js.json.WriteString("\"" + vv.String() + "\"")
 			case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 				reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64:
-				js.json += fmt.Sprintf("%v", vv)
+				js.json.WriteString(fmt.Sprintf("%v", vv))
 			case reflect.Slice:
 				st := tt.Type.Elem()
 
-				js.json += "["
+				js.json.WriteString("[")
 
 				for x := 0; x < vv.Len(); x++ {
 					switch st.Kind() {
 					case reflect.String:
-						js.json += fmt.Sprintf("\"%v\"", vv.Index(x))
+						js.json.WriteString(fmt.Sprintf("\"%v\"", vv.Index(x)))
 					case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 						reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
 						reflect.Float32, reflect.Float64:
-						js.json += fmt.Sprintf("%v", vv.Index(x))
+						js.json.WriteString(fmt.Sprintf("%v", vv.Index(x)))
 					case reflect.Struct:
 						js.serializer(vv.Index(x).Interface())
 					}
 					if x < vv.Len()-1 {
-						js.json += ","
+						js.json.WriteString(",")
 					}
 
 				}
 
-
-			json.WriteString("]")
-
-				js.json += "]"
-
+				js.json.WriteString("]")
 
 			case reflect.Struct:
 				js.serializer(vv.Interface())
 			}
 
-
-		if i < n-1 {
-			json.WriteString(",")
-
 			if i < n-1 {
-				js.json += ","
-			}
+				js.json.WriteString(",")
 
+			}
 		}
 
-
-	json.WriteString("}")
-
-	return json.String(), nil
-
-		js.json += "}"
-
+		js.json.WriteString("}")
 	} else {
 		err := errors.New("type passed is not of type Struct or Struct Pointer")
 		return "", err
 	}
 
-	return js.json, nil
+	return js.json.String(), nil
 }
 
 func jsonFormarter(s string) string {
